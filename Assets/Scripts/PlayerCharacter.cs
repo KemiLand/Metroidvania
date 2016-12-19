e24c329474feca163438a2db1056e0989905ddc9
Mike Nussbaum <Mike Nussbaum>
2016-12-19 22:39:40 +0100

    Added level, character and walking animation. Not done

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCharacter : MonoBehaviour {
+
+    [SerializeField] float speed;
+    [SerializeField] bool isTurnedRight = true;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    public void Move(float horizontal, bool jump)
+    {
+        transform.position += new Vector3(speed * horizontal * Time.deltaTime, 0.0f, 0.0f);
+
+        if (horizontal > 0 && !isTurnedRight)
+        {
+            Flip();
+        }
+        else if (horizontal < 0 && isTurnedRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+
+        isTurnedRight = !isTurnedRight;
+    }
+}
---
+++ Assets/Scripts/UserControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class UserControl : MonoBehaviour {
+
+    PlayerCharacter pChar;
+
+    float horizontal = 0.0f;
+    bool jump = false;
+    Animator walk;
+
+
+    // Use this for initialization
+    void Awake()
+    {//There are no Update before Awake
+        pChar = GetComponent<PlayerCharacter>();
+
+    }
+    void Start()
+    {//There can be Update before Start
+        walk = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        jump = Input.GetButtonDown("Jump");
+        //GetButtonDown, true when clik on button
+        //GetButton, true if pressed
+        //GetButtonUp, true when release button
+
+        horizontal = Input.GetAxis("Horizontal");
+        walk.SetFloat("Speed", Mathf.Abs(horizontal));
+
+        if (InputManager.Devices.Count > 0)
+        {
+            var inputDevice = InputManager.Devices[0];
+            horizontal += inputDevice.LeftStickX;
+        }
+
+        if (Mathf.Abs(horizontal) > 1.0f) //capping horizontal
+        {
+            horizontal = Mathf.Sign(horizontal);
+        }
+
+
+        pChar.Move(horizontal, jump);
+    }
+}
