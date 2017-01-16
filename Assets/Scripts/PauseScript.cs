4d6a3046adcb08a1155d4213dba2c098b1338af3
Mike Nussbaum <Mike Nussbaum>
2017-01-16 05:04:07 +0100

    Added canvas for ui, changed camera script, PauseScript done, needs PauseMenu and mainMenu

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    [SerializeField] GameObject PauseUI;
+    private bool Paused = false;
+
+    void Start()
+    {
+        PauseUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            Paused = !Paused;
+        }
+
+        if (Paused)
+        {
+            PauseUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        if (!Paused)
+        {
+            PauseUI.SetActive(false);
+            Time.timeScale = 1;
+        }
+
+    }
+
+    public void Resume()
+    {
+        Paused = false;
+
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("menu");
+        Paused = false;
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
