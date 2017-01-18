759d0dfbe3ec66ffaecf2dcc0ce17f98cbe0d2bd
Mike Nussbaum <Mike Nussbaum>
2017-01-18 20:34:12 +0100

    Added main menu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+    public void StartGame()
+    {
+        SceneManager.LoadScene("FirstLevel");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
