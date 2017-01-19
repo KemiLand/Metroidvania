1ee5d11575b61f5e810d9d13b184eff31fb3ddcb
Mike Nussbaum <Mike Nussbaum>
2017-01-19 01:36:25 +0100

    Added a victory condition, played when all enemies are dead

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    float timeToEnd = 2;
 
-	// Use this for initialization
-	void Start () {
-	
+    // Use this for initialization
+    void Start ()
+    {
+        
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        var enemies = GameObject.FindWithTag("Enemy");
+
+        if(enemies == null)
+        {
+            if(timeToEnd > 0)
+            {
+                timeToEnd -= Time.deltaTime;
+            }
+            else
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
 	}
 }
